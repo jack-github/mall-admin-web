6ce5ab20afd2ba26b15137e5b232701e3c31b130
李志 <lizhid@youyou.com>
2019-04-10 14:49:21 +0800

    修改配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+    NODE_ENV: '"development"',
+    BASE_API: '"http://10.127.2.50:8081"'
 })
